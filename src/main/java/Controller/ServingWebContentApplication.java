1588981ff45fac5a40809f99e80f65543be68e9e
nkix <nrx0320@outlook.com>
2022-03-05 19:50:00 -0500

    add html page for open-end question create

--- src/main/java/Controller/ServingWebContentApplication.java
+++ src/main/java/Controller/ServingWebContentApplication.java
@@ -2,7 +2,10 @@ package Controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
+@EntityScan(basePackages = "Entity")
 @SpringBootApplication
 public class ServingWebContentApplication {
     public static void main(String[] args){
--- src/main/java/Entity/Answer.java
+++ src/main/java/Entity/Answer.java
@@ -7,6 +7,7 @@ import javax.persistence.Id;
 
 @Entity
 public class Answer implements Question{
+    //open ended questions
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
