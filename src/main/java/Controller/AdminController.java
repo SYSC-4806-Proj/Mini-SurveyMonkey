43a7f3be581d4658fbffb32f0177fef0f38fc9c7
jiawe <jiaweima@cmail.carleton.ca>
2022-03-11 07:17:43 -0500

    Add main page route

--- src/main/java/Controller/AdminController.java
+++ src/main/java/Controller/AdminController.java
@@ -11,46 +11,46 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import javax.swing.text.html.parser.Entity;
-import java.util.ArrayList;
-import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Array;
+import java.util.*;
 
 @Controller
 public class AdminController {
     private QuestionnaireRepo questionnaireRepo;
 
-    public AdminController(QuestionnaireRepo questionnaireRepo){
+    public AdminController(QuestionnaireRepo questionnaireRepo) {
         this.questionnaireRepo = questionnaireRepo;
     }
 
-    @RequestMapping(path="/create", method = RequestMethod.GET)
-    public String questionnaireCreateForm(){
+    @RequestMapping(path = "/create", method = RequestMethod.GET)
+    public String questionnaireCreateForm() {
         return "qCreate";
     }
 
-    @RequestMapping(path="create", method = RequestMethod.POST)
-    public String questionnaireCreate(@RequestBody MultiValueMap<String, String> formData){
-        Questionnaire questionnaire = new Questionnaire();
+//    @RequestMapping(path="create", method = RequestMethod.POST)
+//    public String questionnaireCreate(@RequestBody MultiValueMap<String, String> formData){
+//        Questionnaire questionnaire = new Questionnaire();
+//
+//        for(List<String> value: formData.values()){
+//                for(String content: value){
+//                    Range q = new Range();
+//                    q.setQuestion(content);
+//                    questionnaire.addQuestion(q);
+//                    System.out.println(content);
+//                }
+//        }
+//        questionnaireRepo.save(questionnaire);
+//        long id = questionnaire.getId();
+//        return "redirect:/view/" + id;
+//    }
 
-        for(List<String> value: formData.values()){
-                for(String content: value){
-                    Range q = new Range();
-                    q.setQuestion(content);
-                    questionnaire.addQuestion(q);
-                    System.out.println(content);
-                }
-        }
-
-        questionnaireRepo.save(questionnaire);
-        long id = questionnaire.getId();
-
-        return "redirect:/view/" + id;
-    }
 
     @GetMapping("/view/{id}")
     public String viewQuestionnaire(@PathVariable long id, Model model){
         Questionnaire questionnaire = this.questionnaireRepo.findById(id);
-        if (questionnaire == null){
+        if (questionnaire == null) {
             return "qCreate";
         }
         model.addAttribute("questionnaire", questionnaire);
@@ -72,4 +72,47 @@ public class AdminController {
         return "qView";
     }
 
+    @RequestMapping(path = "/create", method = RequestMethod.POST)
+    public String questionnaireCreate(HttpServletRequest request) {
+        Questionnaire questionnaire = new Questionnaire();
+        Map<String, String[]> formData = request.getParameterMap();
+
+        for (Map.Entry<String, String[]> entry : formData.entrySet()) {
+            for (String content : entry.getValue()) {
+                if (entry.getKey().equals("open_end_question")){
+                    OpenEnd q = new OpenEnd();
+                    q.setQuestion(content);
+                    questionnaire.addQuestion(q);
+
+                } else if(entry.getKey().equals("range_question")){
+                    Range q = new Range();
+                    q.setQuestion(content);
+                    questionnaire.addQuestion(q);
+
+                } else if(entry.getKey().equals("selection_question")){
+                    Selection q = new Selection();
+                    q.setQuestion(content);
+                    questionnaire.addQuestion(q);
+
+                }
+            }
+        }
+
+        questionnaireRepo.save(questionnaire);
+        long id = questionnaire.getId();
+        return "redirect:/view/" + id;
+
+    }
+
+    
+
+    //@RequestMapping(path="/create", method = RequestMethod.GET)
+    //public String chooseQuestionType(){
+
+    //}
+
+    @GetMapping("/")
+    public String mainPage(){
+        return "index";
+    }
 }
