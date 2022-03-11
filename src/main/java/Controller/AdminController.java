abcd3b8338683d2ab69bb5ec385c04f110d1afad
Tiantian Lin <lintiantian0702@gmail.com>
2022-03-11 01:16:38 -0500

    [Tian]New Feature: User could choose three question types and add the content to the database

--- src/main/java/Controller/AdminController.java
+++ src/main/java/Controller/AdminController.java
@@ -1,8 +1,6 @@
 package Controller;
 
-import Entity.OpenEnd;
-import Entity.Question;
-import Entity.Questionnaire;
+import Entity.*;
 import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -12,24 +10,24 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpServletRequest;
 import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @Controller
 public class AdminController {
     private QuestionnaireRepo questionnaireRepo;
 
-    public AdminController(QuestionnaireRepo questionnaireRepo){
+    public AdminController(QuestionnaireRepo questionnaireRepo) {
         this.questionnaireRepo = questionnaireRepo;
     }
 
-    @RequestMapping(path="/create", method = RequestMethod.GET)
-    public String questionnaireCreateForm(){
+    @RequestMapping(path = "/create", method = RequestMethod.GET)
+    public String questionnaireCreateForm() {
         return "qCreate";
     }
 
-    @RequestMapping(path="create", method = RequestMethod.POST)
+    /*@RequestMapping(path="create", method = RequestMethod.POST)
     public String questionnaireCreate(@RequestBody MultiValueMap<String, String> formData){
         Questionnaire questionnaire = new Questionnaire();
 
@@ -41,21 +39,57 @@ public class AdminController {
                     System.out.println(content);
                 }
         }
-
         questionnaireRepo.save(questionnaire);
         long id = questionnaire.getId();
-
         return "redirect:/view/" + id;
     }
-
-    @RequestMapping(path="/view/{id}", method = RequestMethod.GET)
-    public String viewQuestionnaire(@PathVariable long id, Model model){
+*/
+    @RequestMapping(path = "/view/{id}", method = RequestMethod.GET)
+    public String viewQuestionnaire(@PathVariable long id, Model model) {
         Questionnaire questionnaire = this.questionnaireRepo.findById(id);
-        if (questionnaire == null){
+        if (questionnaire == null) {
             return "qCreate";
         }
         model.addAttribute("questionnaire", questionnaire);
         return "qView";
     }
+
+    @RequestMapping(path = "/create", method = RequestMethod.POST)
+    public String questionnaireCreate(HttpServletRequest request) {
+        Questionnaire questionnaire = new Questionnaire();
+        Map<String, String[]> formData = request.getParameterMap();
+
+        for (Map.Entry<String, String[]> entry : formData.entrySet()) {
+            for (String content : entry.getValue()) {
+                if (entry.getKey().equals("open_end_question")){
+                    OpenEnd q = new OpenEnd();
+                    q.setQuestion(content);
+                    questionnaire.addQuestion(q);
+
+                } else if(entry.getKey().equals("range_question")){
+                    Range q = new Range();
+                    q.setQuestion(content);
+                    questionnaire.addQuestion(q);
+
+                } else if(entry.getKey().equals("selection_question")){
+                    Selection q = new Selection();
+                    q.setQuestion(content);
+                    questionnaire.addQuestion(q);
+
+                }
+            }
+        }
+
+        questionnaireRepo.save(questionnaire);
+        long id = questionnaire.getId();
+        return "redirect:/view/" + id;
+
+    }
+
+    //@RequestMapping(path="/create", method = RequestMethod.GET)
+    //public String chooseQuestionType(){
+
+    //}
+
 
 }
